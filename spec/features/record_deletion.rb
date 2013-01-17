5ab6784903cdaddc75bb4fd20638d35097bd8cb9
Aurelien Benel <aurelien.benel@utt.fr>
2013-01-17 19:55:58 +0100

    TEST: Delete a record.

---
+++ spec/features/record_deletion.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'Delete a record' do
+
+  $a_title = a_string()
+
+  background do
+    visit '/'
+    click_on '+' 
+    fill_in 'title', :with => $a_title
+    fill_in 'issued', :with => '1885'
+    select 'invitation', :from => 'aeresType'    
+    click_on 'Enregistrer'    
+  end
+
+  scenario 'for any record' do
+    click_on 'Supprimer...'
+    page.should have_content 'Voulez-vous supprimer cette notice ?'
+    in_dialog.click_button 'Supprimer'
+    page.should have_content 'Publications (références)'
+    page.should_not have_content $a_title
+  end
+
+end

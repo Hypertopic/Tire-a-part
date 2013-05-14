e0d03d2d08100cce5e7f6b81c6de751f880a500b
Aurelien Benel <aurelien.benel@utt.fr>
2013-05-14 17:46:47 +0200

    FIX: Testability with user agents that don't specify preferred languages.

--- localization.js
+++ localization.js
@@ -2,11 +2,14 @@
     
 function localized() {
   var available = "en";
-  for each (var l in req.headers["Accept-Language"].split(",")) {
-    var preferred = l.substring(0,2);
-    if (i18n.hasOwnProperty(preferred)) {
-      available = preferred;
-      break;
+  var required = req.headers["Accept-Language"];
+  if (required) {
+    for each (var l in required.split(",")) {
+      var preferred = l.substring(0,2);
+      if (i18n.hasOwnProperty(preferred)) {
+        available = preferred;
+        break;
+      }
     }
   }
   return i18n[available];

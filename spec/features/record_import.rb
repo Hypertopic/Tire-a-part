d2bb6469d3d7849f6bcc1d927547652f64c55479
Aurelien Benel <aurelien.benel@utt.fr>
2013-05-27 15:50:59 +0200

    TEST: Import a record from DBLP.
    Without proceedings data for now.

--- spec/features/record_import.rb
+++ spec/features/record_import.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 feature 'Import a record' do
 
-  scenario 'from ACM' do
+  background do
     visit '/'
     click_on 'Créer...'
     click_on 'Importer...'
+  end
+
+  scenario 'from ACM' do
     fill_in 'bibtex', :with => sample('acm')
     in_dialog.click_button 'Importer'
     page.should have_field 'creator', :with => 'Frédéric Merle, Aurélien Bénel, Guillaume Doyen, Dominique Gaïti'
@@ -13,6 +16,19 @@ feature 'Import a record' do
     page.should have_field 'ispartof', :with => 'Proceedings of the 17th ACM international conference on Supporting group work'
     page.should have_field 'pages', :with => '117--120'
     page.should have_field 'publisher', :with => 'ACM'
+    page.should have_field 'issued', :with => '2012'
+    page.should have_field 'url', :with => 'http://doi.acm.org/10.1145/2389176.2389195'
+    # No abstract
+  end
+
+  scenario 'from DBLP' do
+    fill_in 'bibtex', :with => sample('dblp')
+    click_on 'Importer'
+    page.should have_field 'creator', :with => 'Frédéric Merle, Aurélien Bénel, Guillaume Doyen, Dominique Gaïti'
+    page.should have_field 'title', :with => 'Decentralized documents authoring system for decentralized teamwork: matching architecture with organizational structure'
+    page.should have_field 'ispartof', :with => 'GROUP'
+    page.should have_field 'pages', :with => '117-120'
+    #page.should have_field 'publisher', :with => 'ACM'
     page.should have_field 'issued', :with => '2012'
     page.should have_field 'url', :with => 'http://doi.acm.org/10.1145/2389176.2389195'
     # No abstract

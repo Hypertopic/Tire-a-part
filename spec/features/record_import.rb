ec221f51679c992e3c60b6908294cc85d7794bc0
Aurelien Benel <aurelien.benel@utt.fr>
2013-05-28 09:48:12 +0200

    TEST: Import a record from Google Scholar.

--- spec/features/record_import.rb
+++ spec/features/record_import.rb
@@ -47,5 +47,17 @@ feature 'Import a record' do
     field('abstract').should == 'While systems for collaborative distributed works focus on enhancing distributed work group productivity, little attention has been paid to their architecture. In fact, most of these systems rely on centralized ones for both user communications and data hosting. These architectures raise issues about the administrative control, maintenance and management of the central entity. In this paper, we present a new architecture based on peer-to-peer (P2P) model driven by user relationship. In our architecture, users choose the trusted co-workers they are connected with. Thus, only the most trusted users manage to obtain a high number of connections which grant them a relative authority inside the system. Copyright © 2012 by the Association for Computing Machinery, Inc. (ACM).'
   end
 
+  scenario 'from Google Scholar' do
+    fill_in 'bibtex', :with => sample('google')
+    in_dialog.click_button 'Importer'
+    field('creator').should == 'Frédéric Merle, Aurélien Bénel, Guillaume Doyen, Dominique Gaïti'
+    field('title').should == 'Decentralized documents authoring system for decentralized teamwork: matching architecture with organizational structure'
+    field('ispartof').should == 'Proceedings of the 17th ACM international conference on Supporting group work'
+    field('pages').should == '117--120'
+    field('publisher').should == 'ACM'
+    field('issued').should == '2012'
+    # No URI nor DOI
+    # No abstract
+  end
 
 end

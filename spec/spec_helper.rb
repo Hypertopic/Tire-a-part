919716383b55c431a4fc51f064a7d0a3877f4629
Bruniaux <f.bruniaux@gmail.com>
2014-06-13 12:14:14 +0200

    Change Capybara.default_wait_time default value
    Test with value = 3s

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'capybara/rspec'
 require 'capybara/webkit'
 
-Capybara.default_wait_time = 5
+Capybara.default_wait_time = 3
 Capybara.run_server = false
 Capybara.default_driver = :webkit
 Capybara.app_host = 

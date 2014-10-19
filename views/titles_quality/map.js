3bb5f198fe5bdbee8c275ed44e4d31f1aa740f06
Aurelien Benel <aurelien.benel@utt.fr>
2014-10-19 15:01:30 +0200

    FIX: Unpublished papers should not appear in quality checks.

--- views/titles_quality/map.js
+++ views/titles_quality/map.js
@@ -1,13 +1,15 @@
 function(o) {
   // !code lib/string.js
-  var title = o['DC.title'].trim().toLowerCase(),
-    ispartof = o['DC.relation.ispartof'];
-  emit(['', title], ispartof);
-  emit([':by', title], ispartof); // workaround for a rewrite bug
-  for each (var c in o['DC.creator']) {
-    emit([normalize(c), title], ispartof);
-  }
-  for each (var a in o.affiliation) {
-    emit([normalize(a), title], ispartof);
+  if (o['DC.issued']) {
+    var title = o['DC.title'].trim().toLowerCase(),
+      ispartof = o['DC.relation.ispartof'];
+    emit(['', title], ispartof);
+    emit([':by', title], ispartof); // workaround for a rewrite bug
+    for each (var c in o['DC.creator']) {
+      emit([normalize(c), title], ispartof);
+    }
+    for each (var a in o.affiliation) {
+      emit([normalize(a), title], ispartof);
+    }
   }
 }

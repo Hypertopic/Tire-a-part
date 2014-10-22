21e98eb3ba2e3ee72864757cd04a92585625cfa0
Aurelien Benel <aurelien.benel@utt.fr>
2014-10-22 10:45:46 +0200

    FIX: Check creator when the fullname has only one part.

--- views/creators_quality/map.js
+++ views/creators_quality/map.js
@@ -2,10 +2,9 @@ function(o) {
   // !code lib/string.js
   for each (var author in o['DC.creator']) {
     if (author) {
-      var fullname = author.normalize(),
-        n = fullname.indexOf(" "),
-        surname = fullname.slice(n+1),
-        firstname = fullname.slice(0, n);
+      var name_parts = author.normalize().split(/\s/),
+        firstname = name_parts.shift(),
+        surname = name_parts.join(' ');
       emit(['', surname, firstname]);
       emit([':by', surname, firstname]); // workaround for a rewrite bug
       for each (var a in o.affiliation) {

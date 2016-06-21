Hypertopic/Tire-a-part

commit 300143ea19af9c7b55854b32510df28030ba5486

FIX: Settings changes should be applied on-the-fly.
Warning: When the change has impact on indexes 
or is processed by libraries (e.g. bibtex export),
the code will still need to be pushed.

lists/dashboard.js
@@ -1,5 +1,4 @@
 function(head, req) {
-  // !json settings.categories  
   var Mustache = require("lib/mustache");
   const BY = req.query.by;
   const SINCE = req.query.since;
@@ -14,7 +13,7 @@ function(head, req) {
   });
   var row;
   var lastYear = null;
-  var data = settings.categories;
+  var data = this.settings.categories;
   data.unshift("");
   while (row = getRow()) {
     var year = row.key[1];

lists/activity.js
@@ -2,7 +2,6 @@ function(head, req) {
   // !code localization.js
   // !code lib/string.js
   // !code lib/record.js
-  // !json settings
   var Mustache = require("lib/mustache");
   const BY = req.query.by;
   const SINCE = req.query.since;
@@ -73,13 +72,13 @@ function(head, req) {
     types.push(typeData);
   }
   var programs = [];
-  for each (p in settings.programs) {
+  for each (p in this.settings.programs) {
     programs.push({key: p.normalize(), value: p});
   }
   return Mustache.to_html(this.templates.activity_html, {
     query: req.query,
     i18n: localized(),
-    groups: settings.groups,
+    groups: this.settings.groups,
     programs: programs,
     types: types
   }, this.templates.partials);

lists/creators_quality.js
@@ -1,7 +1,6 @@
 function(head, req) {  
   // !code localization.js
   // !code lib/string.js
-  // !json settings
   var Mustache = require("lib/mustache");
   start({
     headers: {"Content-Type": "text/html;charset=utf-8"}
@@ -16,13 +15,13 @@ function(head, req) {
     });
   }
   var programs = [];
-  for each (p in settings.programs) {
+  for each (p in this.settings.programs) {
     programs.push({key: p.normalize(), value: p});
   }
   return Mustache.to_html(this.templates.creators_quality, {
     query: req.query,
     i18n: localized(),
-    groups: settings.groups,
+    groups: this.settings.groups,
     programs: programs,
     creators: creators
   }, this.templates.partials);

lists/titles_quality.js
@@ -1,7 +1,6 @@
 function(head, req) {  
   // !code localization.js
   // !code lib/string.js
-  // !json settings
   var Mustache = require("lib/mustache");
   start({
     headers: {"Content-Type": "text/html;charset=utf-8"}
@@ -16,13 +15,13 @@ function(head, req) {
     });
   }
   var programs = [];
-  for each (p in settings.programs) {
+  for each (p in this.settings.programs) {
     programs.push({key: p.normalize(), value: p});
   }
   return Mustache.to_html(this.templates.titles_quality, {
     query: req.query,
     i18n: localized(),
-    groups: settings.groups,
+    groups: this.settings.groups,
     programs: programs,
     references: references
   }, this.templates.partials);

shows/record.js
@@ -1,10 +1,8 @@
 function(o, req) {
-  // !json templates.record_html
-  // !json templates.record_html_form
-  // !json settings
   // !code localization.js
   // !code lib/record.js
   var Mustache = require("lib/mustache");
+  var settings = this.settings;
 
   function formatAttachments(attachments, paperID) {
     var result = [];
@@ -50,7 +48,7 @@ function(o, req) {
     };
   }
   var template = "record_html" + ((req.query.form=="")?"_form":"");
-  return Mustache.render(templates[template], {
+  return Mustache.render(this.templates[template], {
     id: o._id,
     _rev: o._rev,
     url: o.url,

commit 471d77d4b8329ff9fb10a743a6ce38ecfa1795cd

FIX: Random shift in CSV export header.
Regression caused by 300143ea19af9c7b55854b32510df28030ba5486.

lists/dashboard.js
@@ -12,7 +12,7 @@ function(head, req) {
   });
   var row;
   var lastYear = null;
-  var data = this.settings.categories;
+  var data = this.settings.categories.slice();
   data.unshift("");
   while (row = getRow()) {
     var year = row.key[1];